a559f8fbba390e2130d1160c89af9e27f7a2261f
linngxiao1993 <18397870508@163.com>
2019-09-27 16:26:47 +0800

    22

--- src/main/java/HelloWorldController.java
+++ src/main/java/HelloWorldController.java
@@ -2,5 +2,6 @@ public class HelloWorldController {
     public static void main(String[] args) {
         System.out.print("aa");
         System.out.print("bb");
+        System.out.print("CC");
     }
 }
